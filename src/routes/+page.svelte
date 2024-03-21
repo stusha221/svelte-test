37b33017b432da5a4ea30ce033a49ee025696028
Anastasia <stusha221@gmail.com>
2024-03-21 17:58:59 +0900

    add

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -13,12 +13,6 @@
     <!-- Pages will be injected below -->
      <slot></slot>
     </div>
-    <style>
-    .container {
-    max-width: 1400px; 
-    margin: 50px auto;
-    }
-    </style>
 
 <svelte:head>
 <title>Анастасия Ваулина </title>
@@ -45,3 +39,65 @@ ducimus expedita labore nemo odio quibusdam quidem sapiente soluta. </h2>
 </div>
 <div class='background-div'></div>
 
+<style>
+.container {
+    max-width: 1400px; 
+    margin: 50px auto;
+    }
+	.info {
+	text-align: center; 
+	position: relative;
+	}
+	
+	.info-container {
+	height: 100%; 
+	display: flex;
+	flex-direction: column; 
+	justify-content: center; 
+	text-align: left; 
+	align-items: start;
+	}
+	
+	.container {
+	max-width: 1400px;
+	margin: 190px 0 0 2.5rem;
+	}
+	
+	.info h1{
+	font-size: 4rem; 
+	margin: 0;}
+	
+	.info h1 p {
+	position: relative;
+	font: inherit;
+	margin: 0;
+	text-align: left;
+	line-height: 130%; width: fit-content; font-weight: 800;
+	letter-spacing: -0.03 em;
+	}
+	
+	.info h2 {
+	max-width: 65ch; 
+	font-weight: 200;
+	margin-top: 0.50rem;
+	}
+	.background-div{
+	background-color: Hecebe8;
+	width: 100%; height: 56vh;
+	margin: -205px auto;}
+	
+	.info .info-text {
+	position: relative;
+	}
+	
+	.scroll-button {
+	display: flex; 
+	border: none; 
+	outline: none;
+	cursor: pointer;
+	}
+	
+	.scroll-button.arrow {
+	margin-left: 0.50rem;
+	}
+</style>
