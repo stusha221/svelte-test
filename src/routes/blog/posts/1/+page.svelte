4363e50511fa5c041ce335dabe91ab3571e1fbe1
Anastasia <stusha221@gmail.com>
2024-04-02 00:14:38 +0900

    add

---
+++ src/routes/blog/posts/1/+page.svelte
@@ -0,0 +1,3 @@
+<h1>My First Blog Post</h1>
+p.lorem200 (emmet абзаца с текстом заполнителем)
+p.lorem200 (emmet абзаца с текстом заполнителем)
---
+++ src/routes/blog/posts/2/+page.svelte
@@ -0,0 +1,3 @@
+<h1>My Second Blog Post</h1>
+p.lorem200 (emmet абзаца с текстом заполнителем)
+p.lorem200 (emmet абзаца с текстом заполнителем)
