bebe1c327936f2336cae26ccd6668e745c111679
Anastasia <stusha221@gmail.com>
2024-04-02 00:19:16 +0900

    add

--- src/routes/project/+page.svelte
+++ src/routes/project/+page.svelte
@@ -1,2 +1,24 @@
-<h1>Портфолио</h1>
-todo...
+<svelte:head>
+<title>Портфолио</title>
+</svelte:head>
+<div class='container'>
+<h1>Мои проекты</h1>
+<div class='apps'>
+{#each routes as page}
+<div class='app'>
+<h2>{page.title}</h2>
+<p>{page.body}</p>
+<p class='readmore'>
+<a class='link'
+href={`/project/apps/${page.id}`}>
+Читать далее
+</a>
+</p>
+</div>
+{/each}
+</div>
+</div>
+
+<div class="container">
+    <slot />
+    </div>
