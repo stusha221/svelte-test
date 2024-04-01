fe0c592a545a10bb931bb44d40f62de9b7e561b6
Anastasia <stusha221@gmail.com>
2024-04-01 23:55:07 +0900

    add

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,10 +2,10 @@
     <div class="nav-container">
     <a href="/" class="nav-logo" title="Back to Homepage">Имя Фамилия </a>
      <div class="nav-links">
-		{#if link.isShow}
-<a href={link.href}
-class="link">{link.title}</a>
-{/if}
+		{#each nav as link}
+		<a href={link.href}
+		class="link">{link.title}</a>
+		{/each}
     </div>
     </div>
     </nav>
