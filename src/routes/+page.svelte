02fa82342be5066df75f8cd42972bb61a0c772ca
Anastasia <stusha221@gmail.com>
2024-04-01 23:44:16 +0900

    add

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,10 +2,10 @@
     <div class="nav-container">
     <a href="/" class="nav-logo" title="Back to Homepage">Имя Фамилия </a>
      <div class="nav-links">
-    <a href="/about" class="link">Обо мне</a>
-    <a href="/blog" class="link" >Блог</a>
-    <a href="/projects" class="link">Портфолио </a>
-    <a href="/contact" class="link">Контакты</a>
+		{#each nav as link}
+		<a href={link.href}
+		class="link">{link.title}</a>
+		{/each}
     </div>
     </div>
     </nav>
@@ -39,6 +39,30 @@ ducimus expedita labore nemo odio quibusdam quidem sapiente soluta. </h2>
 </div>
 <div class='background-div'></div>
 
+<script>
+	const nav = [
+	{
+	title: 'Обо мне',
+	href: '/about'
+	},
+	{
+	title: 'Блог',
+	href: '/blog'
+	},
+	{
+	title: 'Портфолио',
+	href: '/project'
+	},
+	{
+	title: 'Контакты',
+	href: '/contact'
+	},
+	{
+	title: 'temp',
+	href: '/temp'
+	}
+	]
+	</script>
 <style>
 	.info {
 	text-align: center; 
