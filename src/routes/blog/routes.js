3318187b0739a013463957e1f25494c55e01c019
Anastasia <stusha221@gmail.com>
2024-04-02 00:06:41 +0900

    add

---
+++ src/routes/blog/routes.js
@@ -0,0 +1,7 @@
+export const routes = [
+    {
+    id: 1,
+    title: 'My First Blog Post',
+    body: 'Lorem ipsum dolor sitamet, consectetur adipisicing elit. Blanditiis dignissimos dolorem eaex maiores minima minus nisi perspiciatis sapiente voluptate',
+    },
+    ];
